07d9273ade989e77e8eef1e528b6cea93fae51f2
Dagmawi Kassa <141551998+DagmawiKK@users.noreply.github.com>
2024-07-01 19:08:13 +0300

    Time: 13 ms (60.41%) | Memory: 11.7 MB (26.98%) - LeetSync

---
+++ 290-word-pattern/word-pattern.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def wordPattern(self, pattern, s):
+        """
+        :type pattern: str
+        :type s: str
+        :rtype: bool
+        """
+        dict = {}
+        j = 0
+        list = s.split(" ")
+        if len(pattern) != len(list):
+            return False
+        for i in range(0, len(pattern)):
+            if pattern[i] not in dict.keys() and list[j] not in dict.values():
+                dict[pattern[i]] = list[j]
+                j += 1
+            else:
+                if dict.get(pattern[i]) != list[j]:
+                    return False
+                else:
+                    j += 1
+        print(dict)
+        return True
