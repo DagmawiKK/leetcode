773606be33b414a0a1db4c46454c29447b5bc1a9
Dagmawi Kassa <141551998+DagmawiKK@users.noreply.github.com>
2024-07-01 19:42:27 +0300

    Time: 52 ms (23.68%) | Memory: 16.9 MB (6.58%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.py
+++ 1293-three-consecutive-odds/three-consecutive-odds.py
@@ -7,4 +7,6 @@ class Solution:
                 if((len(arr)-1 - arr.index(i)) >= 2):
                      if arr[arr.index(i)+1] %2 !=0 and arr[arr.index(i)+2] %2 !=0:
                         return True
+                else:
+                    return False
         return False
