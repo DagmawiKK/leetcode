8f355fee20363cf9e2ac483fcc9c386d5fa635c8
Dagmawi Kassa <141551998+DagmawiKK@users.noreply.github.com>
2024-07-06 15:39:11 +0300

    Time: 42 ms (27.95%) | Memory: 16.4 MB (84.32%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -4,7 +4,7 @@ class Solution:
             count = 0
             m = 0
             for i in range(len(string)-1, -1, -1):
-                if(string[i] == "1"):
+                if string[i] in ("1"):
                     m = m + (2**count)
                 count += 1
             return m
