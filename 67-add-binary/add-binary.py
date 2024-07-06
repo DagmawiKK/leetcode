7ca0303c3547d7bc27cca2b765bca368e8586e19
Dagmawi Kassa <141551998+DagmawiKK@users.noreply.github.com>
2024-07-06 15:32:51 +0300

    Time: 34 ms (79.40%) | Memory: 16.4 MB (97.81%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -3,8 +3,8 @@ class Solution:
         def binary(string):
             count = 0
             m = 0
-            for i in reversed(string):
-                if(int(i) == 1):
+            for i in range(len(string)-1, -1, -1):
+                if(int(string[i]) == 1):
                     m = m + (2**count)
                 count += 1
             return m
