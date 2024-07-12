75cda705f5f1e6010f335011ec3728ab743a528b
Dagmawi Kassa <141551998+DagmawiKK@users.noreply.github.com>
2024-07-12 16:28:48 +0300

    Time: 301 ms (10.17%) | Memory: 16.9 MB (85.41%) - LeetSync

--- 2502-sort-the-people/sort-the-people.py
+++ 2502-sort-the-people/sort-the-people.py
@@ -3,9 +3,11 @@ class Solution:
         n = len(names)
         
         for i in range(n):
-            for j in range(0, n - i - 1):
-                if heights[j] < heights[j + 1]:
-                    heights[j], heights[j + 1] = heights[j + 1], heights[j]
-                    names[j], names[j + 1] = names[j + 1], names[j]
-        
+            max_idx = i
+            for j in range(i + 1, n):
+                if heights[j] > heights[max_idx]:
+                    max_idx = j
+            
+            heights[i], heights[max_idx] = heights[max_idx], heights[i]
+            names[i], names[max_idx] = names[max_idx], names[i]
         return names
