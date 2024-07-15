a3009525441c0485953ca8cc5c755ce22b826654
Dagmawi Kassa <141551998+DagmawiKK@users.noreply.github.com>
2024-07-15 10:17:03 +0300

    Time: 40 ms (93.22%) | Memory: 16.7 MB (25.19%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -1,17 +1,15 @@
+from collections import Counter
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        if len(nums1) == 0 or len(nums2) == 0:
-            return []
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
         
-        def worker(larger, smaller):
-            result = []
-            for item in smaller:
-                if item in larger:
-                    result.append(item)
-                    larger[larger.index(item)] = '-'
-            return result
-        if(len(nums1) <= len(nums2)):
-            return worker(nums2, nums1)
-        else:
-            return worker(nums1, nums2)
-        return result 
+        count = Counter(nums1)
+        result = []
+        
+        for num in nums2:
+            if count[num] > 0:
+                result.append(num)
+                count[num] -= 1
+                
+        return result
