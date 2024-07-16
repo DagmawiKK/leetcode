002fb21d7390f8207173aeff0eac89d94846a99e
Dagmawi Kassa <141551998+DagmawiKK@users.noreply.github.com>
2024-07-16 20:41:06 +0300

    Time: 54 ms (19.86%) | Memory: 16.6 MB (7.29%) - LeetSync

---
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.py
@@ -0,0 +1,17 @@
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        nums.sort()
+        result = []
+        try:
+            result.append(nums.index(target))
+        except ValueError:
+            return []
+
+        val = nums.index(target) + 1
+
+        if val < len(nums):
+        
+            while val < len(nums)and nums[val] == target :
+                result.append(result[-1] + 1)
+                val += 1
+        return result
