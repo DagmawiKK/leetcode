2ae6a1cdde51c524fd0b0afbc59889526197e116
Dagmawi Kassa <141551998+DagmawiKK@users.noreply.github.com>
2024-07-18 19:42:37 +0300

    Time: 46 ms (75.11%) | Memory: 18.7 MB (14.33%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.py
@@ -4,17 +4,28 @@ class Solution:
         pt1 = 0
         pt2 = len(s) - 1 
         i = 0
-        vowles = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u']
+        vowels = { 
+        'a': "Vowel", 
+        'e': "Vowel", 
+        'i': "Vowel", 
+        'o': "Vowel", 
+        'u': "Vowel", 
+        'A': "Vowel", 
+        'E': "Vowel", 
+        'I': "Vowel", 
+        'O': "Vowel", 
+        'U': "Vowel"
+    } 
         while pt1 <= pt2 and i < len(strList):
-            if strList[pt1] in vowles and strList[pt2] not in vowles:
+            if vowels.get(strList[pt1], "notVowle") == "Vowel" and vowels.get(strList[pt2], "notVowle") != "Vowel":
                 pt2 -=1
-            elif strList[pt1] not in vowles and strList[pt2] in vowles:
+            elif vowels.get(strList[pt1], "notVowle") != "Vowel" and vowels.get(strList[pt2], "notVowle") == "Vowel":
                 pt1 += 1
 
-            elif strList[pt1] not in vowles and strList[pt2] not in vowles:
+            elif vowels.get(strList[pt1], "notVowle") != "Vowel" and vowels.get(strList[pt2], "notVowle") != "Vowel":
                 pt2 -=1
                 pt1 +=1
-            elif strList[pt1] in vowles and strList[pt2] in vowles:
+            elif vowels.get(strList[pt1], "notVowle") == "Vowel" and vowels.get(strList[pt2], "notVowle") == "Vowel":
                 strList[pt1], strList[pt2] = strList[pt2], strList[pt1]
                 pt2 -=1
                 pt1 +=1
