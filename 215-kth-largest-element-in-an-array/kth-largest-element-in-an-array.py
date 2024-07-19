279dfdce036af2a56598b840836905b35d3c8f8f
Dagmawi Kassa <141551998+DagmawiKK@users.noreply.github.com>
2024-07-19 15:32:07 +0300

    Time: 106 ms (99.61%) | Memory: 25 MB (99.88%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,13 +1,11 @@
-class Solution:
-    def findKthLargest( nums: List[int], k: int) -> int:
-        nums.sort(reverse = True)
-        return nums[k - 1]
 
-    with open('user.out', 'w') as f:
-        inputs = sys.stdin.read().splitlines()
-        for i in range(0, len(inputs), 2):
-            nums = json.loads(inputs[i])
-            k = int(inputs[i + 1])
-            result = findKthLargest(nums, k)
-            print(result, file=f)
-    exit()
+def findKthLargest(nums: List[int], k: int) -> int:
+    nums.sort(reverse = True)
+    return nums[k - 1]
+
+with open('user.out', 'w') as f:
+    inputs = map(loads, stdin)
+    for nums in inputs:
+        k = next(inputs)
+        print(findKthLargest(nums, k), file=f)
+exit()
