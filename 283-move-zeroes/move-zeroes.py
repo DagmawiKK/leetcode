44b11ebbf949f0e748bee569d96e8eed5bbbef8d
Dagmawi Kassa <141551998+DagmawiKK@users.noreply.github.com>
2024-07-19 14:27:26 +0300

    Time: 130 ms (40.11%) | Memory: 18 MB (50.89%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,12 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        # [1, 2, 0, 3, 4, 0, 5]
-        # j = 0
-        # i = 0
-
         j = 0
-        k = len(nums) - 1
+
         for i in range(len(nums)):
             if nums[i] != 0:
                 nums[j], nums[i] = nums[i], nums[j]
