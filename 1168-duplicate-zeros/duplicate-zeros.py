9509828964033070ac63e89aebda8a9c7e526a50
Dagmawi Kassa <141551998+DagmawiKK@users.noreply.github.com>
2024-07-21 19:40:49 +0300

    Time: 56 ms (85.95%) | Memory: 17.2 MB (86.22%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.py
+++ 1168-duplicate-zeros/duplicate-zeros.py
@@ -3,14 +3,13 @@ class Solution:
         """
         Do not return anything, modify arr in-place instead.
         """
-        if 0 in set(arr):
+        if 0 in arr:
             i = 0
             while i < len(arr):
                 if arr[i] == 0 and i < len(arr)-1:
                     arr.insert(i + 1, 0)
                     arr.pop()
-                    i += 2
-                else:
                     i += 1
+                i += 1
 
 
