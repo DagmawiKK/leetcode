802d553617e6d7db4d830f2af1366c17b43a147b
Dagmawi Kassa <141551998+DagmawiKK@users.noreply.github.com>
2024-07-21 17:28:53 +0300

    Time: 44 ms (6.51%) | Memory: 16.6 MB (14.10%) - LeetSync

---
+++ 2481-strictly-palindromic-number/strictly-palindromic-number.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+
+        def converter(n, i):
+            if n == 0:
+                return '0'
+            nums = []
+            while n:
+                n, r = divmod(n, i)
+                nums.append(str(r))
+            return ''.join(reversed(nums))
+        for i in range(2, n-1):
+            string_rep = converter(n, i)
+            left = 0
+            right = len(string_rep) - 1
+            if len(string_rep) % 2 != 0: return False
+            while left < right:
+                if string_rep[left] != string_rep[right]:
+                    return False
+                left += 1
+                right -=1
+        return True
