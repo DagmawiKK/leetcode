f8f42d5cb3ca8de15d0349fd0c638f32498573a8
Dagmawi Kassa <141551998+DagmawiKK@users.noreply.github.com>
2024-07-21 19:54:39 +0300

    Time: 35 ms (64.27%) | Memory: 16.6 MB (15.62%) - LeetSync

--- 2561-number-of-distinct-averages/number-of-distinct-averages.py
+++ 2561-number-of-distinct-averages/number-of-distinct-averages.py
@@ -1,11 +1,9 @@
 class Solution:
     def distinctAverages(self, nums: List[int]) -> int:
-        nums.sort()
-
         avg = set()
 
         while len(nums) > 1:
-            avg.add((nums[-1] + nums[0])/2)
-            nums.pop()
-            nums.pop(0)
+            avg.add((max(nums) + min(nums))/2)
+            nums.pop(nums.index(min(nums)))
+            nums.pop(nums.index(max(nums)))
         return len(avg)
