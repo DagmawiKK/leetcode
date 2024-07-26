b738fc30251b6378607144169bf3f57965b63811
Dagmawi Kassa <141551998+DagmawiKK@users.noreply.github.com>
2024-07-26 16:54:24 +0300

    Time: 60 ms (54.24%) | Memory: 28.5 MB (41.82%) - LeetSync

--- 187-repeated-dna-sequences/repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/repeated-dna-sequences.py
@@ -1,15 +1,13 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
         
-        arr = []
-        result = []
+        checked = set()
+        result = set()
         
         for i in range(0, len(s)):
-            current_seq = s[i:i+10]
-            k = i+1
-            arr.append(current_seq)
-        count = Counter(arr)
-        for i in count:
-            if count[i] > 1:
-                result.append(i)
-        return result
+            if s[i:i+10] not in checked:
+                checked.add(s[i:i+10])
+            else:
+                result.add(s[i:i+10])
+        
+        return list(result)
