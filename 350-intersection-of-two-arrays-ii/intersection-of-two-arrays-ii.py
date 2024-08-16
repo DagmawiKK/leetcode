d9bb768a28b879fb6a7aa20cb723ce96077969b7
Dagmawi Kassa <141551998+DagmawiKK@users.noreply.github.com>
2024-08-16 14:47:25 +0300

    Time: 45 ms (76.20%) | Memory: 16.7 MB (25.95%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.py
@@ -1,16 +1,14 @@
-from collections import Counter
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        if len(nums1) > len(nums2):
-            nums1, nums2 = nums2, nums1
+        count = Counter(nums2)
 
         result = []
-        
 
-        # the above lines are equivalent to:
-        for num in nums2:
-            if num in nums1:
-                result.append(num)
-                nums1[nums1.index(num)] = '-' # Set it to some arbrtriary value that isn't in nums2
-                
+        for i in nums1:
+            if count[i] > 0:
+                result.append(i)
+                count[i] -= 1
+
         return result
+            
+
