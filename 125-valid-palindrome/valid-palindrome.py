ed20e49157c0b03f40f8425ab687be96b7171e47
Dagmawi Kassa <141551998+DagmawiKK@users.noreply.github.com>
2024-08-16 15:28:11 +0300

    Time: 41 ms (77.63%) | Memory: 22.9 MB (5.11%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,6 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        array = list(map(str.lower, list(filter(lambda x: x.isalpha() or x.isnumeric(), s))))
+        array = list(map(str.lower, filter(lambda x: x.isalpha() or x.isnumeric(), s)))
         print(array)
 
         return "".join(array) == "".join(reversed(array))
