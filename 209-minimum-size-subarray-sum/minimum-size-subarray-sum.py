6588362a8819e0e56af625f066abde29a6fb52ad
Dagmawi Kassa <141551998+DagmawiKK@users.noreply.github.com>
2024-08-17 14:48:38 +0300

    Time: 183 ms (94.59%) | Memory: 30.2 MB (86.60%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -1,7 +1,7 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         left = 0
-        result = 10**9
+        result = float('inf')
         total = 0 
         for right in range(len(nums)):
             total += nums[right]
@@ -10,7 +10,7 @@ class Solution:
                 result = min(result, right - left+1)
                 left += 1
                 
-        if result == 10**9:
+        if result == float('inf'):
             return 0
 
         return result
