422c6de338dfe1fd33df63e35a00bd298b7231d6
Dagmawi Kassa <141551998+DagmawiKK@users.noreply.github.com>
2024-08-17 14:48:03 +0300

    Time: 183 ms (94.59%) | Memory: 30.3 MB (20.29%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.py
@@ -5,12 +5,10 @@ class Solution:
         total = 0 
         for right in range(len(nums)):
             total += nums[right]
-            if total >= target:
-                result = min(result, right - left+1)
             while total >= target:
                 total -= nums[left]
+                result = min(result, right - left+1)
                 left += 1
-                result = min(result, right - left+2)
                 
         if result == 10**9:
             return 0
