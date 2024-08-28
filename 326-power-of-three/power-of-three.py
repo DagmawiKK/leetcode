ca741fd93d5c9262ae4f55ddfa4ad84605812963
Dagmawi Kassa <141551998+DagmawiKK@users.noreply.github.com>
2024-08-28 19:23:54 +0300

    Time: 106 ms (6.37%) | Memory: 16.6 MB (50.73%) - LeetSync

--- 326-power-of-three/power-of-three.py
+++ 326-power-of-three/power-of-three.py
@@ -1,8 +1,13 @@
 class Solution:
+    def __init__(self):
+        self.seen = {}
     def isPowerOfThree(self, n: int) -> bool:
         if n == 1:
             return True
         elif n < 3:
             return False
+        elif n in self.seen:
+            return seen[n]
         
-        return self.isPowerOfThree(n / 3)
+        self.seen[n] = self.isPowerOfThree(n / 3)
+        return self.seen[n]
