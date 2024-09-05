8c22df597e131119106b76a960004dd6d629d0ef
Dagmawi Kassa <141551998+DagmawiKK@users.noreply.github.com>
2024-09-05 14:46:58 +0300

    Time: 160 ms (44.44%), Space: 18 MB (55.68%) - LeetHub

--- 0978-valid-mountain-array/0978-valid-mountain-array.py
+++ 0978-valid-mountain-array/0978-valid-mountain-array.py
@@ -9,22 +9,16 @@ class Solution:
         if arr[0] == maxx or arr[right] == maxx:
             return False
         while left < right:
-            if arr[left] < arr[left + 1]:
-                left += 1
-            else:
-                if arr[left] != maxx:
+            if arr[left] != maxx:
+                if arr[left] < arr[left+1]:
+                    left += 1
+                else:
                     return False
-                elif arr[right] == maxx and arr[right-1] ==  maxx:
+            if arr[right] != maxx:
+                if arr[right] < arr[right - 1]:
+                    right -= 1
+                else:
                     return False
-
-            if arr[right] < arr[right-1]:
-                right-= 1
-            else:
-                if arr[right] != maxx:
-                    return False
-                elif arr[right] == maxx and arr[right-1] ==  maxx:
-                    return False
-        return True
 
 
         return True
