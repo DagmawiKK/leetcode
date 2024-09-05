9833315eeff3b98cbc5698ddf6878c06606e02e6
Dagmawi Kassa <141551998+DagmawiKK@users.noreply.github.com>
2024-09-05 16:16:52 +0300

    Time: 104 ms (49.31%), Space: 17.1 MB (64.72%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.py
+++ 2502-sort-the-people/2502-sort-the-people.py
@@ -1,15 +1,3 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        def bubble(heights, names):
-            unsorted_until_index = len(heights) - 1
-            sortedd = False
-            while not sortedd:
-                sortedd = True
-                for i in range(unsorted_until_index):
-                    if heights[i] < heights[i+1]:
-                        sortedd = False
-                        heights[i], heights[i + 1] = heights[i + 1], heights[i]
-                        names[i], names[i+1] = names[i+1], names[i]
-                unsorted_until_index -= 1
-            return names
-        return bubble(heights, names)
+        return [name for height, name in sorted(zip(heights, names), reverse=True)]
