f123458b7cb78e1151276b7ece34b23c349fe710
Dagmawi Kassa <141551998+DagmawiKK@users.noreply.github.com>
2024-09-11 21:59:27 +0300

    Time: 39 ms (78.35%), Space: 17.6 MB (46.2%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -1,14 +1,14 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         answer = [0] * n
-        for i in range(n):
-            if (i+1) % 3 == 0 and (i+1) % 5 == 0:
-                answer[i] = "FizzBuzz"
-            elif (i+1) % 3 == 0:
-                answer[i] = "Fizz"
-            elif (i+1) % 5 == 0:
-                answer[i] = "Buzz"
+        for i in range(1, n+1):
+            if (i) % 3 == 0 and (i) % 5 == 0:
+                answer[i-1] = "FizzBuzz"
+            elif (i) % 3 == 0:
+                answer[i-1] = "Fizz"
+            elif (i) % 5 == 0:
+                answer[i-1] = "Buzz"
             else:
-                answer[i] = str(i + 1)
+                answer[i-1] = str(i)
         
         return answer
