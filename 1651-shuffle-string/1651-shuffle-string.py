f70c19e10d1ed15ca417c604d76098280b0cc0ee
Dagmawi Kassa <141551998+DagmawiKK@users.noreply.github.com>
2024-09-25 22:08:11 +0300

    Time: 81 ms (5.13%), Space: 21.3 MB (30.2%) - LeetHub

--- 1651-shuffle-string/1651-shuffle-string.py
+++ 1651-shuffle-string/1651-shuffle-string.py
@@ -1,9 +1,5 @@
+from more_itertools import sort_together
 class Solution:
     def restoreString(self, s: str, indices: List[int]) -> str:
-        # res = [0] * len(s)
-        # for i, index in enumerate(indices):
-        #     res[index] = s[i] 
-        # return "".join(res) 
-
-        arr = sorted(zip(list(s), indices), key = lambda x: x[1])
-        return "".join([x[0] for x in arr])
+        result = sort_together([indices, list(s)])
+        return ''.join(result[1])
